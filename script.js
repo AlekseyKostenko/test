bcc5ffc5f7f9ea95f625e63229baccb61697756b
Grevil94 <kostenko.201019914@mail.ru>
2018-07-23 18:33:06 +0300

    Добавлена нижняя карусель

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 $('.carousel').carousel({
     interval: 9000000
 })
+
+function Slider(){
+    var col_main_left=-403;
+    $("#right_nav").click(function(){
+        if(col_main_left!==-5239){
+            col_main_left = col_main_left-403;
+            $("#slider_box>ul").css("-webkit-transition", 0.5+"s");
+            $("#slider_box>ul").css("-moz-transition", 0.5+"s");
+            $("#slider_box>ul").css("transition", 0.5+"s");
+        } else if (col_main_left==-5239) {
+            col_main_left = 0;
+            $("#slider_box>ul").css("-webkit-transition", 0.01+"s");
+            $("#slider_box>ul").css("-moz-transition", 0.01+"s");
+            $("#slider_box>ul").css("transition", 0.01+"s");
+        }
+        $("#slider_box>ul").css("margin-left",col_main_left+"px");
+    })
+
+    $("#left_nav").click(function(){
+        if(col_main_left!==-403 || col_main_left==0){
+            $("#slider_box>ul").css("-webkit-transition", 0.5+"s");
+            $("#slider_box>ul").css("-moz-transition", 0.5+"s");
+            $("#slider_box>ul").css("transition", 0.5+"s");
+            col_main_left = col_main_left+403;
+            $("#slider_box>ul").css("margin-left",col_main_left+"px")
+        } else if (col_main_left==-403) {  
+            $("#slider_box>ul").css("-webkit-transition", 0.5+"s");
+            $("#slider_box>ul").css("-moz-transition", 0.5+"s");
+            $("#slider_box>ul").css("transition", 0.5+"s");
+            col_main_left = col_main_left+403;
+            $("#slider_box>ul").css("margin-left",col_main_left+"px");
+            col_main_left=-5239;
+            $("#slider_box>ul").css("-webkit-transition", 0.01+"s");
+            $("#slider_box>ul").css("-moz-transition", 0.01+"s");
+            $("#slider_box>ul").css("transition", 0.01+"s");
+            $("#slider_box>ul").css("margin-left",col_main_left+"px");
+        }
+       // $("#slider_box>ul").css("margin-left",col_main_left+"px");
+    })
+}
+$(Slider());
+
