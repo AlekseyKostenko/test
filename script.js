e72bc50e5283525e79c316f70a094d7077dd9007
Grevil94 <kostenko.201019914@mail.ru>
2018-07-25 14:26:44 +0300

    Добавлена прилипалка, нижняя карусель работает

--- script.js
+++ script.js
@@ -5,13 +5,22 @@ $('.carousel').carousel({
 function Slider(){
     var col_main_left=-403;
     $("#right_nav").click(function(){
-        if(col_main_left!==-5239){
+        if(col_main_left!==-4836){
             col_main_left = col_main_left-403;
-           // $("#slider_box>ul").css("transition", 0.5+"s");
-        } else if (col_main_left==-5239) {
+            $("#slider_box>ul").removeClass("animoff").addClass("anim");
+            $("#slider_box>ul").css("margin-left",col_main_left+"px");
+        } else if (col_main_left==-4836) {
+            col_main_left = col_main_left-403;
+            $("#slider_box>ul").removeClass("animoff").addClass("anim");
+            $("#slider_box>ul").css("margin-left",col_main_left+"px");
+
+            setTimeout(function(){
             col_main_left = 0;
+            $("#slider_box>ul").removeClass("anim").addClass("animoff");
+            $("#slider_box>ul").css("margin-left",col_main_left+"px");
+            },500)
         }
-        $("#slider_box>ul").css("margin-left",col_main_left+"px");
+
     })
 
     $("#left_nav").click(function(){
@@ -23,12 +32,29 @@ function Slider(){
             col_main_left = col_main_left+403;
             $("#slider_box>ul").removeClass("animoff").addClass("anim");
             $("#slider_box>ul").css("margin-left",col_main_left+"px");
-            col_main_left=-5239;
-            $("#slider_box>ul").removeClass("anim").addClass("animoff");
-            $("#slider_box>ul").css("margin-left",col_main_left+"px");
+
+            setTimeout(function(){
+                col_main_left=-5239;
+                $("#slider_box>ul").removeClass("anim").addClass("animoff");
+                $("#slider_box>ul").css("margin-left",col_main_left+"px");
+            },500)  
         }
        // $("#slider_box>ul").css("margin-left",col_main_left+"px");
     })
 }
 $(Slider());
 
+
+function HeadTop (){ 
+    var headblock = $("#headblock"); //Получаем нужный объект var
+    topOfheadblock = $(headblock).offset().top; //Получаем начальное расположение нашего блока 
+    $(window).scroll(function () { var windowScroll = $(window).scrollTop(); //Получаем величину, показывающую на сколько прокручено окно 
+        if (windowScroll > topOfheadblock){ // Если прокрутили больше, чем расстояние до блока, то приклеиваем его
+            $(headblock).removeClass("col-sm-12").addClass("topWindow"); 
+        } else {
+            $(headblock).removeClass("topWindow").addClass("col-sm-12");
+        };      
+    });
+};
+
+$(HeadTop());
